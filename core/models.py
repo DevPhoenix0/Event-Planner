f7c83e132c4da979557b23bbf99deb9d39d8dc47
Seif <seiftamer606@gmail.com>
2026-03-09 21:46:10 +0200

    Add initial Event model and migration files

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Event(models.Model):
+    id = models.UUIDField(primary_key=True,editable=False)
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True,blank=True)
+    location = models.CharField(max_length=255)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
